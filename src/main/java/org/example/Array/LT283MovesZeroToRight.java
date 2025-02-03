6d9b42b0af3a3924bf4b1301e3216b9301107178
Arpan Patel <arpanpatel1992@gmail.com>
2025-02-03 16:58:07 -0500

    Arrays Leetcode problems

---
+++ src/main/java/org/example/Array/LT283MovesZeroToRight.java
@@ -0,0 +1,49 @@
+package org.example.Array;
+
+import java.util.Arrays;
+
+/**
+ * Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+ *
+ * Note that you must do this in-place without making a copy of the array.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: nums = [0,1,0,3,12]
+ * Output: [1,3,12,0,0]
+ */
+public class LT283MovesZeroToRight {
+
+    void moveToRight(int[] nums) {
+        int i = 0, j = -1;
+
+        //step 1: identify first zero
+        while( i < nums.length) {
+            if(nums[i] == 0) {
+                j = i;
+                break;
+            }
+            i++;
+        }
+
+        //step 2: iterate through all remaining elements from pivot and shuffle zero to right side.
+        i = j + 1;
+        while ( i < nums.length) {
+            if(nums[i] != 0) {
+                nums[j] = nums[i];
+                nums[i] = 0;
+                j++;
+            }
+            i++;
+        }
+    }
+
+    public static void main(String[] args) {
+        var lt = new LT283MovesZeroToRight();
+        int nums[] = {4,4,0,1};
+        lt.moveToRight(nums);
+        Arrays.stream(nums).forEach(System.out::println);
+    }
+}
---
+++ src/main/java/org/example/DP/HRMaxSubsequenceSort.java
@@ -0,0 +1,21 @@
+package org.example.DP;
+
+public class HRMaxSubsequenceSort {
+
+    boolean subseqnceSort(String baseStr, String queStr, int m, int n) {
+//        boolean[][] dp = new boolean[m+1][n+1];
+//        for(int i = 0 ; i < m +1; i++) {
+//            for(int j = 0 ; j < n +1; j ++) {
+//                if( i ==0) dp[i][j] = false;
+//                if( j ==0) dp[i][j] = false;
+//                if( i ==0 && j ==0) dp[i][j] = true;
+//            }
+//        }
+//        for(int i = 0 ; i < m +1; i++) {
+//            for (int j = 0; j < n + 1; j++) {
+//
+//            }
+//        }
+        return  false;
+    }
+}
